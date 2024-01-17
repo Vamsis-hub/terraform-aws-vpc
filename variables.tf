0d7e848a8f9f6606b2150a831ac81ee664213888
vamsis-hub <vamsireddychaganti.19@gmail.com>
2024-01-17 22:53:55 +1000

    terraform-vpc

--- variables.tf
+++ variables.tf
@@ -58,3 +58,40 @@ variable "private_subnets_tags"{
     default = {}
   
 } 
+variable "database_subnets_cidr"{
+    type = list
+  validation {
+    condition = length(var.database_subnets_cidr) == 2
+    error_message = "Please give 2 database valid subnet CIDR"
+  } 
+}
+
+variable "database_subnets_tags"{
+   default = {}
+} 
+
+variable "nat_gateway_tags" {
+ default = {}
+}
+
+variable "public_route_table_tags" {
+  default = {}
+}
+
+variable "private_route_table_tags" {
+  default = {}
+}
+
+variable "database_route_table_tags" {
+  default = {}
+}
+
+variable "is_peering_required" {
+  default = false
+}
+variable "acceptor_vpc_id" {
+  default = ""
+}
+variable "vpc_peering_tags" {
+  default = {}
+}
