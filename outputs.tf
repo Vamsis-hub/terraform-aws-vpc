11c256386855b0890b6d4b8eff1c8c8aabf13daa
vamsis-hub <vamsireddychaganti.19@gmail.com>
2024-01-20 17:52:13 +1000

    terraform-vpc

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,19 @@ output "azs" {
   value = data.aws_availability_zones.azs.names
 
 }
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public_subnets[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private_subnets[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database_subnet[*].id
+}
